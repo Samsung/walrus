099f06c93f98fa8dd9b6768d7999a81d4e1c5b40
HyukWoo Park <hyukwoo.park@samsung.com>
2023-06-09 15:39:02 +0900

    Update check_tidy script to use python3 in default
    Signed-off-by: HyukWoo Park <hyukwoo.park@samsung.com>

--- tools/check_tidy.py
+++ tools/check_tidy.py
@@ -69,7 +69,7 @@ def check_tidy(src_dir, update, clang_format, stats):
             formatted = subprocess.check_output([clang_format, '-style=file', file])
 
             if update:
-                with open(file, 'w') as f:
+                with open(file, 'wb') as f:
                     f.write(formatted)
 
             stats.files += 1
@@ -90,7 +90,7 @@ def check_tidy(src_dir, update, clang_format, stats):
                 if not line.strip():
                     stats.empty_lines += 1
 
-            diff = list(unified_diff(original, formatted.splitlines(True)))
+            diff = list(unified_diff(original, formatted.decode('utf-8').splitlines(True)))
             if diff:
                 report_error('format error')
                 for diffline in diff:
