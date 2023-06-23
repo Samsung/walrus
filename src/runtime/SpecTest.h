5a0f476ac3f26e3b75a753a9da70d0bcbf78dc2f
Adam Laszlo Kulcsar <kuladam@inf.u-szeged.hu>
2023-06-23 12:06:03 +0200

    Implement base for Walrus WASI
    Implement basic structure of wasi functions and also update test runner
    to run wasi tests with the 'wasi' argument.
    
    Signed-off-by: Adam Laszlo Kulcsar <kuladam@inf.u-szeged.hu>

---
+++ src/runtime/SpecTest.h
@@ -0,0 +1,132 @@
+/*
+ * Copyright (c) 2023-present Samsung Electronics Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "Walrus.h"
+
+namespace Walrus {
+
+class SpecTestFunctionTypes {
+    MAKE_STACK_ALLOCATED();
+
+public:
+    enum Index : uint8_t {
+        // The R is meant to represent the results, after R are the result types.
+        NONE = 0,
+        I32R,
+        RI32,
+        I64R,
+        F32R,
+        F64R,
+        I32F32R,
+        F64F64R,
+        INVALID,
+        INDEX_NUM,
+    };
+
+    SpecTestFunctionTypes()
+    {
+        m_vector.reserve(INDEX_NUM);
+        size_t index = 0;
+
+        ValueTypeVector* param;
+        ValueTypeVector* result;
+
+        {
+            // NONE
+            param = new ValueTypeVector();
+            result = new ValueTypeVector();
+            m_vector[index++] = new FunctionType(param, result);
+        }
+        {
+            // I32R
+            param = new ValueTypeVector();
+            result = new ValueTypeVector();
+            param->push_back(Value::Type::I32);
+            m_vector[index++] = new FunctionType(param, result);
+        }
+        {
+            // RI32
+            param = new ValueTypeVector();
+            result = new ValueTypeVector();
+            result->push_back(Value::Type::I32);
+            m_vector[index++] = new FunctionType(param, result);
+        }
+        {
+            // I64
+            param = new ValueTypeVector();
+            result = new ValueTypeVector();
+            param->push_back(Value::Type::I64);
+            m_vector[index++] = new FunctionType(param, result);
+        }
+        {
+            // F32
+            param = new ValueTypeVector();
+            result = new ValueTypeVector();
+            param->push_back(Value::Type::F32);
+            m_vector[index++] = new FunctionType(param, result);
+        }
+        {
+            // F64
+            param = new ValueTypeVector();
+            result = new ValueTypeVector();
+            param->push_back(Value::Type::F64);
+            m_vector[index++] = new FunctionType(param, result);
+        }
+        {
+            // I32F32
+            param = new ValueTypeVector();
+            result = new ValueTypeVector();
+            param->push_back(Value::Type::I32);
+            param->push_back(Value::Type::F32);
+            m_vector[index++] = new FunctionType(param, result);
+        }
+        {
+            // F64F64
+            param = new ValueTypeVector();
+            result = new ValueTypeVector();
+            param->push_back(Value::Type::F64);
+            param->push_back(Value::Type::F64);
+            m_vector[index++] = new FunctionType(param, result);
+        }
+        {
+            // INVALID
+            param = new ValueTypeVector();
+            result = new ValueTypeVector();
+            param->push_back(Value::Type::Void);
+            m_vector[index++] = new FunctionType(param, result);
+        }
+
+        ASSERT(index == INDEX_NUM);
+    }
+
+    ~SpecTestFunctionTypes()
+    {
+        for (size_t i = 0; i < m_vector.size(); i++) {
+            delete m_vector[i];
+        }
+    }
+
+    FunctionType* operator[](const size_t idx)
+    {
+        ASSERT(idx < m_vector.size());
+        return m_vector[idx];
+    }
+
+private:
+    FunctionTypeVector m_vector;
+};
+
+} // namespace Walrus
---
+++ src/wasi/Wasi.h
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2023-present Samsung Electronics Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "Walrus.h"
+#include "runtime/Value.h"
+#include "runtime/Function.h"
+#include "runtime/ObjectType.h"
+#include "runtime/SpecTest.h"
+
+namespace Walrus {
+
+class WASI {
+public:
+    WASI();
+
+    ~WASI()
+    {
+    }
+
+    struct WasiFunc {
+        std::string name;
+        SpecTestFunctionTypes::Index functionType;
+        ImportedFunction::ImportedFunctionCallback ptr;
+    };
+
+#define FOR_EACH_WASI_FUNC(F) \
+    F(test, NONE)             \
+    F(printI32, I32R)         \
+    F(writeI32, RI32)         \
+    F(proc_exit, I32R)
+
+    enum WasiFuncName : size_t {
+#define DECLARE_FUNCTION(NAME, FUNCTYPE) NAME##FUNC,
+        FOR_EACH_WASI_FUNC(DECLARE_FUNCTION)
+#undef DECLARE_FUNCTION
+            FuncEnd,
+    };
+
+    void fillWasiFuncTable();
+    WasiFunc* find(std::string funcName);
+
+    static void test(ExecutionState& state, const uint32_t argc, Value* argv, Value* result, void* data);
+    static void printI32(ExecutionState& state, const uint32_t argc, Value* argv, Value* result, void* data);
+    static void writeI32(ExecutionState& state, const uint32_t argc, Value* argv, Value* result, void* data);
+    static void proc_exit(ExecutionState& state, const uint32_t argc, Value* argv, Value* result, void* data);
+
+    WasiFunc m_wasiFunctions[FuncEnd];
+};
+
+} // namespace Walrus
