70d72192001de9109cf493db7f8a1126c033ab2a
HyukWoo Park <hyukwoo.park@samsung.com>
2023-07-10 23:09:52 +0900

    Update SIMD load zero instructions
    Signed-off-by: HyukWoo Park <hyukwoo.park@samsung.com>

--- third_party/wabt/include/wabt/walrus/binary-reader-walrus.h
+++ third_party/wabt/include/wabt/walrus/binary-reader-walrus.h
@@ -153,9 +153,11 @@ public:
     virtual void OnEndExpr() = 0;
     virtual void OnUnreachableExpr() = 0;
     virtual void EndFunctionBody(Index index) = 0;
+    virtual void OnSimdLaneOpExpr(int opcode, uint64_t value) = 0;
     virtual void OnSimdLoadLaneExpr(int opcode, Index memidx, Address alignment_log2, Address offset, uint64_t value) = 0;
     virtual void OnSimdStoreLaneExpr(int opcode, Index memidx, Address alignment_log2, Address offset, uint64_t value) = 0;
     virtual void OnLoadSplatExpr(int opcode, Index memidx, Address alignmentLog2, Address offset) = 0;
+    virtual void OnLoadZeroExpr(int opcode, Index memidx, Address alignmentLog2, Address offset) = 0;
 
     bool shouldContinueToGenerateByteCode() const
     {
