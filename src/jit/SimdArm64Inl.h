a795aa607fe461707d26cf072d8bcabc443a9c5e
Gergo Csizi <gergocs@inf.u-szeged.hu>
2023-08-02 11:32:34 +0200

    Add ARM64 SIMD Integer Add, Subtract, Negate and Mul operations (#128)
    Signed-off-by: Gergo Csizi gergocs@inf.u-szeged.hu

--- src/jit/SimdArm64Inl.h
+++ src/jit/SimdArm64Inl.h
@@ -18,13 +18,40 @@
 
 namespace SimdOp {
 
+constexpr uint8_t sizeOffset = 22;
+
 enum Type : uint32_t {
-    fabs = 0x4ee0f800,
-    fmin = 0x4ee0f400,
-    fmax = 0x4e60f400,
+    fabs = 0x4ea0f800,
+    fmin = 0x4ea0f400,
+    fmax = 0x4e20f400,
+    add = 0x4e208400,
+    addp = 0x4e20bc00,
+    neg = 0x6e20b800,
+    mul = 0x4e209c00,
+    rev64 = 0x4e200800,
+    shll = 0x2e213800,
+    sqadd = 0x4e200c00,
+    sqsub = 0x4e202c00,
+    sub = 0x6e208400,
+    umlal = 0x2e208000,
+    uqadd = 0x6e200c00,
+    uqsub = 0x6e202c00,
+    xtn = 0xe212800,
 };
 
+enum IntSizeType : uint32_t {
+    B16 = 0x0 << sizeOffset,
+    H8 = 0x1 << sizeOffset,
+    S4 = 0x2 << sizeOffset,
+    D2 = 0x3 << sizeOffset,
 };
+
+enum FloatSizeType : uint32_t {
+    FS4 = 0x0 << sizeOffset,
+    FD2 = 0x1 << sizeOffset,
+};
+
+}; // namespace SimdOp
 
 static void simdEmitOp(sljit_compiler* compiler, uint32_t opcode, sljit_s32 rd, sljit_s32 rn, sljit_s32 rm)
 {
@@ -39,6 +66,20 @@ static void simdEmitOp(sljit_compiler* compiler, uint32_t opcode, sljit_s32 rd, 
     sljit_emit_op_custom(compiler, &opcode, sizeof(uint32_t));
 }
 
+static void simdEmitI64x2Mul(sljit_compiler* compiler, sljit_s32 rd, sljit_s32 rn, sljit_s32 rm)
+{
+    auto tmpReg1 = SLJIT_FR2;
+    auto tmpReg2 = SLJIT_FR3;
+
+    simdEmitOp(compiler,  SimdOp::rev64 | SimdOp::S4, tmpReg2, rm, 0);
+    simdEmitOp(compiler, SimdOp::mul | SimdOp::S4, tmpReg2, tmpReg2, rn);
+    simdEmitOp(compiler, SimdOp::xtn | SimdOp::S4, tmpReg1, rn, 0);
+    simdEmitOp(compiler, SimdOp::addp | SimdOp::S4, rd, tmpReg2, tmpReg2);
+    simdEmitOp(compiler, SimdOp::xtn | SimdOp::S4, tmpReg2, rm, 0);
+    simdEmitOp(compiler, SimdOp::shll | SimdOp::S4, rd, rd, 0);
+    simdEmitOp(compiler, SimdOp::umlal | SimdOp::S4, rd, tmpReg2, tmpReg1);
+}
+
 static void emitUnarySIMD(sljit_compiler* compiler, Instruction* instr)
 {
     Operand* operands = instr->operands();
@@ -49,6 +90,18 @@ static void emitUnarySIMD(sljit_compiler* compiler, Instruction* instr)
     switch (instr->opcode()) {
     case ByteCode::F64X2AbsOpcode:
         type = SLJIT_SIMD_MEM_FLOAT | SLJIT_SIMD_MEM_ELEM_64;
+        break;
+    case ByteCode::I8X16NegOpcode:
+        type = SLJIT_SIMD_MEM_ELEM_8;
+        break;
+    case ByteCode::I16X8NegOpcode:
+        type = SLJIT_SIMD_MEM_ELEM_16;
+        break;
+    case ByteCode::I32X4NegOpcode:
+        type = SLJIT_SIMD_MEM_ELEM_32;
+        break;
+    case ByteCode::I64X2NegOpcode:
+        type = SLJIT_SIMD_MEM_ELEM_64;
         break;
     default:
         ASSERT_NOT_REACHED();
@@ -62,7 +115,19 @@ static void emitUnarySIMD(sljit_compiler* compiler, Instruction* instr)
 
     switch (instr->opcode()) {
     case ByteCode::F64X2AbsOpcode:
-        simdEmitOp(compiler, SimdOp::fabs, dst, args[0].arg, 0);
+        simdEmitOp(compiler, SimdOp::fabs | SimdOp::FD2, dst, args[0].arg, 0);
+        break;
+    case ByteCode::I8X16NegOpcode:
+        simdEmitOp(compiler, SimdOp::neg | SimdOp::B16, dst, args[0].arg, 0);
+        break;
+    case ByteCode::I16X8NegOpcode:
+        simdEmitOp(compiler, SimdOp::neg | SimdOp::H8, dst, args[0].arg, 0);
+        break;
+    case ByteCode::I32X4NegOpcode:
+        simdEmitOp(compiler, SimdOp::neg | SimdOp::S4, dst, args[0].arg, 0);
+        break;
+    case ByteCode::I64X2NegOpcode:
+        simdEmitOp(compiler, SimdOp::neg | SimdOp::D2, dst, args[0].arg, 0);
         break;
     default:
         ASSERT_NOT_REACHED();
@@ -86,6 +151,33 @@ static void emitBinarySIMD(sljit_compiler* compiler, Instruction* instr)
     case ByteCode::F64X2MaxOpcode:
         type = SLJIT_SIMD_MEM_FLOAT | SLJIT_SIMD_MEM_ELEM_64;
         break;
+    case ByteCode::I8X16AddOpcode:
+    case ByteCode::I8X16SubOpcode:
+    case ByteCode::I8X16AddSatSOpcode:
+    case ByteCode::I8X16AddSatUOpcode:
+    case ByteCode::I8X16SubSatSOpcode:
+    case ByteCode::I8X16SubSatUOpcode:
+        type = SLJIT_SIMD_MEM_ELEM_8;
+        break;
+    case ByteCode::I16X8AddOpcode:
+    case ByteCode::I16X8SubOpcode:
+    case ByteCode::I16X8MulOpcode:
+    case ByteCode::I16X8AddSatSOpcode:
+    case ByteCode::I16X8AddSatUOpcode:
+    case ByteCode::I16X8SubSatSOpcode:
+    case ByteCode::I16X8SubSatUOpcode:
+        type = SLJIT_SIMD_MEM_ELEM_16;
+        break;
+    case ByteCode::I32X4AddOpcode:
+    case ByteCode::I32X4SubOpcode:
+    case ByteCode::I32X4MulOpcode:
+        type = SLJIT_SIMD_MEM_ELEM_32;
+        break;
+    case ByteCode::I64X2AddOpcode:
+    case ByteCode::I64X2SubOpcode:
+    case ByteCode::I64X2MulOpcode:
+        type = SLJIT_SIMD_MEM_ELEM_64;
+        break;
     default:
         ASSERT_NOT_REACHED();
         break;
@@ -99,11 +191,70 @@ static void emitBinarySIMD(sljit_compiler* compiler, Instruction* instr)
 
     switch (instr->opcode()) {
     case ByteCode::F64X2MinOpcode:
-        simdEmitOp(compiler, SimdOp::fmin, dst, args[0].arg, args[1].arg);
+        simdEmitOp(compiler, SimdOp::fmin | SimdOp::FD2, dst, args[0].arg, args[1].arg);
         break;
     case ByteCode::F64X2MaxOpcode:
-        simdEmitOp(compiler, SimdOp::fmax, dst, args[0].arg, args[1].arg);
+        simdEmitOp(compiler, SimdOp::fmax | SimdOp::FD2, dst, args[0].arg, args[1].arg);
         break;
+    case ByteCode::I8X16AddOpcode:
+        simdEmitOp(compiler, SimdOp::add | SimdOp::B16, dst, args[0].arg, args[1].arg);
+        break;
+    case ByteCode::I8X16SubOpcode:
+        simdEmitOp(compiler, SimdOp::sub | SimdOp::B16, dst, args[0].arg, args[1].arg);
+        break;
+    case ByteCode::I8X16AddSatSOpcode:
+        simdEmitOp(compiler, SimdOp::sqadd | SimdOp::B16, dst, args[0].arg, args[1].arg);
+        break;
+    case ByteCode::I8X16AddSatUOpcode:
+        simdEmitOp(compiler, SimdOp::uqadd | SimdOp::B16, dst, args[0].arg, args[1].arg);
+        break;
+    case ByteCode::I8X16SubSatSOpcode:
+        simdEmitOp(compiler, SimdOp::sqsub | SimdOp::B16, dst, args[0].arg, args[1].arg);
+        break;
+    case ByteCode::I8X16SubSatUOpcode:
+        simdEmitOp(compiler, SimdOp::uqsub | SimdOp::B16, dst, args[0].arg, args[1].arg);
+        break;
+    case ByteCode::I16X8AddOpcode:
+        simdEmitOp(compiler, SimdOp::add | SimdOp::H8, dst, args[0].arg, args[1].arg);
+        break;
+    case ByteCode::I16X8SubOpcode:
+        simdEmitOp(compiler, SimdOp::sub | SimdOp::H8, dst, args[0].arg, args[1].arg);
+        break;
+    case ByteCode::I16X8MulOpcode:
+        simdEmitOp(compiler, SimdOp::mul | SimdOp::H8, dst, args[0].arg, args[1].arg);
+        break;
+    case ByteCode::I16X8AddSatSOpcode:
+        simdEmitOp(compiler, SimdOp::sqadd | SimdOp::H8, dst, args[0].arg, args[1].arg);
+        break;
+    case ByteCode::I16X8AddSatUOpcode:
+        simdEmitOp(compiler, SimdOp::uqadd | SimdOp::H8, dst, args[0].arg, args[1].arg);
+        break;
+    case ByteCode::I16X8SubSatSOpcode:
+        simdEmitOp(compiler, SimdOp::sqsub | SimdOp::H8, dst, args[0].arg, args[1].arg);
+        break;
+    case ByteCode::I16X8SubSatUOpcode:
+        simdEmitOp(compiler, SimdOp::uqsub | SimdOp::H8, dst, args[0].arg, args[1].arg);
+        break;
+    case ByteCode::I32X4AddOpcode:
+        simdEmitOp(compiler, SimdOp::add | SimdOp::S4, dst, args[0].arg, args[1].arg);
+        break;
+    case ByteCode::I32X4SubOpcode:
+        simdEmitOp(compiler, SimdOp::sub | SimdOp::S4, dst, args[0].arg, args[1].arg);
+        break;
+    case ByteCode::I32X4MulOpcode:
+        simdEmitOp(compiler, SimdOp::mul | SimdOp::S4, dst, args[0].arg, args[1].arg);
+        break;
+    case ByteCode::I64X2AddOpcode:
+        simdEmitOp(compiler, SimdOp::add | SimdOp::D2, dst, args[0].arg, args[1].arg);
+        break;
+    case ByteCode::I64X2SubOpcode:
+        simdEmitOp(compiler, SimdOp::sub | SimdOp::D2, dst, args[0].arg, args[1].arg);
+        break;
+    case ByteCode::I64X2MulOpcode: {
+        simdEmitI64x2Mul(compiler, dst, args[0].arg, args[1].arg);
+        break;
+    }
+
     default:
         ASSERT_NOT_REACHED();
         break;
