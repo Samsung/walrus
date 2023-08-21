b8c4fea996e481a29d805d1ed35545149bc6213a
Gergo <gergocs@inf.u-szeged.hu>
2023-08-21 09:35:37 +0200

    Add ARM64 SIMD Other simple operations
    Signed-off-by: Gergo Csizi gergocs@inf.u-szeged.hu

--- src/jit/ByteCodeParser.cpp
+++ src/jit/ByteCodeParser.cpp
@@ -1019,6 +1019,7 @@ static void createInstructionList(JITCompiler* compiler, ModuleFunction* functio
         case ByteCode::I16X8ExtmulHighI8X16UOpcode:
         case ByteCode::I16X8NarrowI32X4SOpcode:
         case ByteCode::I16X8NarrowI32X4UOpcode:
+        case ByteCode::I16X8Q15mulrSatSOpcode:
         case ByteCode::I32X4AddOpcode:
         case ByteCode::I32X4SubOpcode:
         case ByteCode::I32X4MulOpcode:
@@ -1087,6 +1088,7 @@ static void createInstructionList(JITCompiler* compiler, ModuleFunction* functio
             break;
         }
         case ByteCode::I8X16NegOpcode:
+        case ByteCode::I8X16AllTrueOpcode:
         case ByteCode::I16X8NegOpcode:
         case ByteCode::I16X8ExtaddPairwiseI8X16SOpcode:
         case ByteCode::I16X8ExtaddPairwiseI8X16UOpcode:
@@ -1094,6 +1096,7 @@ static void createInstructionList(JITCompiler* compiler, ModuleFunction* functio
         case ByteCode::I16X8ExtendHighI8X16SOpcode:
         case ByteCode::I16X8ExtendLowI8X16UOpcode:
         case ByteCode::I16X8ExtendHighI8X16UOpcode:
+        case ByteCode::I16X8AllTrueOpcode:
         case ByteCode::I32X4NegOpcode:
         case ByteCode::I32X4ExtaddPairwiseI16X8SOpcode:
         case ByteCode::I32X4ExtaddPairwiseI16X8UOpcode:
@@ -1101,7 +1104,13 @@ static void createInstructionList(JITCompiler* compiler, ModuleFunction* functio
         case ByteCode::I32X4ExtendHighI16X8SOpcode:
         case ByteCode::I32X4ExtendLowI16X8UOpcode:
         case ByteCode::I32X4ExtendHighI16X8UOpcode:
+        case ByteCode::I32X4AllTrueOpcode:
+        case ByteCode::I32X4TruncSatF32X4SOpcode:
+        case ByteCode::I32X4TruncSatF32X4UOpcode:
+        case ByteCode::I32X4TruncSatF64X2SZeroOpcode:
+        case ByteCode::I32X4TruncSatF64X2UZeroOpcode:
         case ByteCode::I64X2NegOpcode:
+        case ByteCode::I64X2AllTrueOpcode:
         case ByteCode::F32X4AbsOpcode:
         case ByteCode::F32X4CeilOpcode:
         case ByteCode::F32X4FloorOpcode:
@@ -1122,7 +1131,8 @@ static void createInstructionList(JITCompiler* compiler, ModuleFunction* functio
         case ByteCode::F64X2PromoteLowF32X4Opcode:
         case ByteCode::F64X2ConvertLowI32X4SOpcode:
         case ByteCode::F64X2ConvertLowI32X4UOpcode:
-        case ByteCode::V128NotOpcode: {
+        case ByteCode::V128NotOpcode:
+        case ByteCode::V128AnyTrueOpcode: {
             group = Instruction::UnarySIMD;
             paramCount = 1;
             break;
