1ad00537177f242fd4b3f7fcd68d232706099f4e
Adam Laszlo Kulcsar <kuladam@inf.u-szeged.hu>
2023-08-28 15:01:17 +0200

    Implement WASI error-handling
    Signed-off-by: Adam Laszlo Kulcsar <kuladam@inf.u-szeged.hu>

--- src/wasi/Wasi.h
+++ src/wasi/Wasi.h
@@ -24,6 +24,96 @@ namespace Walrus {
 
 class WASI {
 public:
+    // type definitions according to preview1
+    // https://github.com/WebAssembly/WASI/blob/main/legacy/preview1/docs.md
+
+#define ERRORS(ERR)                                                   \
+    ERR(success, "no error")                                          \
+    ERR(toobig, "argument list too long")                             \
+    ERR(acces, "permission denied")                                   \
+    ERR(addrinuse, "addres in use")                                   \
+    ERR(addrnotavauil, "addres not abailable")                        \
+    ERR(afnosupport, "address family not supported")                  \
+    ERR(again, "resource unavailable, or operation would block")      \
+    ERR(already, "connection already in progress")                    \
+    ERR(badf, "bad file descriptor")                                  \
+    ERR(badmsg, "bad message")                                        \
+    ERR(busy, "device or resource busy")                              \
+    ERR(canceled, "operation canceled")                               \
+    ERR(child, "no child processes")                                  \
+    ERR(connaborted, "connection aborted")                            \
+    ERR(connrefused, "connection refused")                            \
+    ERR(connresset, "connection reset")                               \
+    ERR(deadlk, "resource deadlock would occur")                      \
+    ERR(destaddrreq, "destination address required")                  \
+    ERR(dom, "mathematics argument out of domain of function")        \
+    ERR(dquot, "reserved")                                            \
+    ERR(exist, "file exists")                                         \
+    ERR(fault, "bad address")                                         \
+    ERR(fbig, "file too large")                                       \
+    ERR(hostunreach, "host unreachable")                              \
+    ERR(idrm, "identifier removed")                                   \
+    ERR(ilseq, "illegal byte sequence")                               \
+    ERR(inprogress, "operation in progress")                          \
+    ERR(intr, "interrupted function")                                 \
+    ERR(inval, "invalid function argument")                           \
+    ERR(io, "io error")                                               \
+    ERR(isconn, "socket is connected")                                \
+    ERR(isdir, "is a directory")                                      \
+    ERR(loop, "too many levels of symbolic links")                    \
+    ERR(mfile, "file descriptor value too large")                     \
+    ERR(mlink, "too many links")                                      \
+    ERR(msgsize, "message too large")                                 \
+    ERR(multihop, "reserved")                                         \
+    ERR(nametoolong, "filename too long")                             \
+    ERR(netdown, "network is down")                                   \
+    ERR(netreset, "connection aborted by network")                    \
+    ERR(netunreach, "network unreachable")                            \
+    ERR(nfile, "too many files open in system")                       \
+    ERR(nobufs, "no buffer space available")                          \
+    ERR(nodev, "no such device")                                      \
+    ERR(noent, "no such file or directory")                           \
+    ERR(noexec, "executable file format error")                       \
+    ERR(nolck, "no locks available")                                  \
+    ERR(nolink, "reserved")                                           \
+    ERR(nomem, "not enough space")                                    \
+    ERR(nomsg, "no message of the desired type")                      \
+    ERR(noprotoopt, "protocol not available")                         \
+    ERR(nospc, "no space left on device")                             \
+    ERR(nosys, "function not supported")                              \
+    ERR(notconn, "the socket is not connected")                       \
+    ERR(notdir, "not a directory or a symbolic link to a directory")  \
+    ERR(notempty, "directory not empty")                              \
+    ERR(notrecoverable, "state not recoverable")                      \
+    ERR(notsock, "not a socket")                                      \
+    ERR(notsup, "not supported or operation not supported in socket") \
+    ERR(notty, "inappropriate I/O control operation")                 \
+    ERR(nxio, "no such device or address")                            \
+    ERR(overflow, "value too large to be stored in data type")        \
+    ERR(ownerdead, "previous owner died")                             \
+    ERR(perm, "operation not permitted")                              \
+    ERR(pipe, "broken pipe")                                          \
+    ERR(proto, "protocol error")                                      \
+    ERR(protonosupport, "protocol not supported")                     \
+    ERR(prototype, "protocol wrong type for socket")                  \
+    ERR(range, "result too large")                                    \
+    ERR(rofst, "read-only file system")                               \
+    ERR(spipe, "invalid seek")                                        \
+    ERR(srch, "no such process")                                      \
+    ERR(stale, "reserved")                                            \
+    ERR(timedout, "connection timed out")                             \
+    ERR(txtbsy, "text file busy")                                     \
+    ERR(xdec, "cross-device link")                                    \
+    ERR(notcapable, "capabilities insufficient")
+
+#define TO_ENUM(ERR, MSG) ERR,
+    typedef enum wasi_errno : uint16_t {
+        ERRORS(TO_ENUM)
+    } wasi_errno_t;
+#undef TO_ENUM
+
+    // end of type definitions
+
     WASI();
 
     ~WASI()
@@ -37,9 +127,6 @@ public:
     };
 
 #define FOR_EACH_WASI_FUNC(F) \
-    F(test, NONE)             \
-    F(printI32, I32R)         \
-    F(writeI32, RI32)         \
     F(proc_exit, I32R)
 
     enum WasiFuncName : size_t {
@@ -52,9 +139,6 @@ public:
     void fillWasiFuncTable();
     WasiFunc* find(std::string funcName);
 
-    static void test(ExecutionState& state, Value* argv, Value* result, void* data);
-    static void printI32(ExecutionState& state, Value* argv, Value* result, void* data);
-    static void writeI32(ExecutionState& state, Value* argv, Value* result, void* data);
     static void proc_exit(ExecutionState& state, Value* argv, Value* result, void* data);
 
     WasiFunc m_wasiFunctions[FuncEnd];
