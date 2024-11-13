f9b94485af76d4a4ca095eaf76c5de97a96cb0bd
Ádám Kulcsár <kuladam@inf.u-szeged.hu>
2024-11-13 13:19:47 +0100

    WIP: Modify test system structure
    Move wamr tests to 'tests/other' directory and restructure the wamr test runner into a standalone file.
    
    Signed-off-by: Ádám Kulcsár <kuladam@inf.u-szeged.hu>

--- tools/run-tests-other.py
+++ tools/run-tests-other.py
@@ -30,8 +30,7 @@ from glob import glob
 from os.path import abspath, basename, dirname, join, relpath
 from shutil import copy
 from subprocess import PIPE, Popen
-
-other_tests = __import__('run-tests-other')
+from unittest import TextTestRunner
 
 PROJECT_SOURCE_DIR = dirname(dirname(abspath(__file__)))
 DEFAULT_WALRUS = join(PROJECT_SOURCE_DIR, 'walrus')
@@ -84,82 +83,81 @@ def _run_wast_tests(engine, files, is_fail):
 
     return fails
 
-@runner('basic-tests', default=True)
-def run_basic_tests(engine):
-    TEST_DIR = join(PROJECT_SOURCE_DIR, 'test', 'basic')
+# @runner('regression', default=True)
+# def run_other_regression_tests(engine):
+#     TEST_DIR = join(PROJECT_SOURCE_DIR, 'test', 'other', 'regression')
+# 
+#     other_test_run_config = json.load(open(join(TEST_DIR, 'running_config.json')))
 
-    print('Running basic tests:')
-    xpass = glob(join(TEST_DIR, '*.wast'))
-    xpass_result = _run_wast_tests(engine, xpass, False)
-
-    tests_total = len(xpass)
-    fail_total = xpass_result
-    print('TOTAL: %d' % (tests_total))
-    print('%sPASS : %d%s' % (COLOR_GREEN, tests_total - fail_total, COLOR_RESET))
-    print('%sFAIL : %d%s' % (COLOR_RED, fail_total, COLOR_RESET))
-
-    if fail_total > 0:
-        raise Exception("basic tests failed")
-
-
-@runner('wasm-test-core', default=True)
-def run_core_tests(engine):
-    TEST_DIR = join(PROJECT_SOURCE_DIR, 'test', 'wasm-spec', 'core')
-
-    print('Running wasm-test-core tests:')
-    xpass = glob(join(TEST_DIR, '**/*.wast'), recursive=True)
-    xpass_result = _run_wast_tests(engine, xpass, False)
-
-    tests_total = len(xpass)
-    fail_total = xpass_result
-    print('TOTAL: %d' % (tests_total))
-    print('%sPASS : %d%s' % (COLOR_GREEN, tests_total - fail_total, COLOR_RESET))
-    print('%sFAIL : %d%s' % (COLOR_RED, fail_total, COLOR_RESET))
-
-    if fail_total > 0:
-        raise Exception("wasm-test-core failed")
-
-
-@runner('wasi', default=True)
-def run_wasi_tests(engine):
-    TEST_DIR = join(PROJECT_SOURCE_DIR, 'test', 'wasi')
-
-    print('Running wasi tests:')
-    xpass = glob(join(TEST_DIR, '*.wast'))
-    xpass_result = _run_wast_tests(engine, xpass, False)
-
-    tests_total = len(xpass)
-    fail_total = xpass_result
-    print('TOTAL: %d' % (tests_total))
-    print('%sPASS : %d%s' % (COLOR_GREEN, tests_total - fail_total, COLOR_RESET))
-    print('%sFAIL : %d%s' % (COLOR_RED, fail_total, COLOR_RESET))
-
-    if fail_total > 0:
-        raise Exception("basic wasi tests failed")
-
-
-@runner('jit', default=True)
-def run_jit_tests(engine):
-    TEST_DIR = join(PROJECT_SOURCE_DIR, 'test', 'jit')
-
-    print('Running jit tests:')
-    xpass = glob(join(TEST_DIR, '*.wast'))
-    xpass_result = _run_wast_tests(engine, xpass, False)
-
-    tests_total = len(xpass)
-    fail_total = xpass_result
-    print('TOTAL: %d' % (tests_total))
-    print('%sPASS : %d%s' % (COLOR_GREEN, tests_total - fail_total, COLOR_RESET))
-    print('%sFAIL : %d%s' % (COLOR_RED, fail_total, COLOR_RESET))
-
-    if fail_total > 0:
-        raise Exception("basic wasm-test-core failed")
-
-@runner('wasm-test-extended', default=True)
+@runner('regression', default=False)
 def run_extended_tests(engine):
-    TEST_DIR = join(PROJECT_SOURCE_DIR, 'test', 'extended')
+    TEST_DIR = join(PROJECT_SOURCE_DIR, 'test', 'other')
 
-    print('Running wasm-extended tests:')
+
+    other_test_run_config = json.load(open(join(TEST_DIR, 'regression/running_config.json')))
+
+    test_list_pass = []
+    test_list_fail = []
+
+    for test_case in other_test_run_config['test cases']:
+        if test_case['deprecated']:
+            continue
+
+        test_paths = sum(
+            [glob(join(TEST_DIR, 'regression', f'issue-{id}', '*.wasm'), recursive=False) for id in test_case['ids']],
+            [])
+
+        if not test_paths:
+            test_paths = sum(
+                [glob(join(TEST_DIR, 'regression', f'issue-{id}', f"{test_case['file']}"), recursive=False) for id in
+                 test_case['ids']],
+                [])
+
+        if ('expected return' not in test_case and test_case['compile_options']['expected return']['ret code'] == 0) \
+                or test_case['expected return']['ret code'] == 0:
+            test_list_pass.extend(test_paths)
+        else:
+            test_list_fail.extend(test_paths)
+
+    print('Running other regression tests:')
+    result = _run_wast_tests(engine, test_list_pass, False)
+    result += _run_wast_tests(engine, test_list_fail, True)
+
+    tests_total = len(test_list_pass) + len(test_list_fail)
+    fail_total = result
+    print('TOTAL: %d' % (tests_total))
+    print('%sPASS : %d%s' % (COLOR_GREEN, tests_total - fail_total, COLOR_RESET))
+    print('%sFAIL : %d%s' % (COLOR_RED, fail_total, COLOR_RESET))
+
+    if fail_total > 0:
+        raise Exception("other regression failed")
+
+@runner('unit', default=True)
+def run_other_unit_tests(engine):
+    TEST_DIR = join(PROJECT_SOURCE_DIR, 'test', 'other', 'unit')
+
+    print('Running other-unit tests:')
+    xfail = [filename for filename in glob(join(TEST_DIR, 'linear-memory-wasm/*.wast'), recursive=False) if
+             ";; Should report an error." in open(filename).read()]
+    xfail.extend([filename for filename in glob(join(TEST_DIR, 'memory64/*exceed*.wat'), recursive=False)])
+    xpass = [filename for filename in glob(join(TEST_DIR, '**/*.wa*t'), recursive=True) if filename not in xfail]
+    xpass_result = _run_wast_tests(engine, xpass, False)
+    xpass_result += _run_wast_tests(engine, xfail, True)
+
+    tests_total = len(xpass) + len(xfail)
+    fail_total = xpass_result
+    print('TOTAL: %d' % (tests_total))
+    print('%sPASS : %d%s' % (COLOR_GREEN, tests_total - fail_total, COLOR_RESET))
+    print('%sFAIL : %d%s' % (COLOR_RED, fail_total, COLOR_RESET))
+
+    if fail_total > 0:
+        raise Exception("other unit failed")
+
+@runner('proposal', default=True)
+def run_extended_tests(engine):
+    TEST_DIR = join(PROJECT_SOURCE_DIR, 'test', 'other', 'proposal_features')
+
+    print('Running other proposal_features tests:')
     xpass = glob(join(TEST_DIR, '**/*.wast'), recursive=True)
     xpass_result = _run_wast_tests(engine, xpass, False)
 
@@ -170,11 +168,39 @@ def run_extended_tests(engine):
     print('%sFAIL : %d%s' % (COLOR_RED, fail_total, COLOR_RESET))
 
     if fail_total > 0:
-        raise Exception("wasm-test-extended failed")
+        raise Exception("other proposal_features failed")
 
-@runner('other', default=False)
-def run_other_tests(engine):
-    other_tests.run_from_other_file(engine, jit)
+@runner('standalone', default=True)
+def run_extended_tests(engine):
+    TEST_DIR = join(PROJECT_SOURCE_DIR, 'test', 'other', 'standalone')
+
+    print('Running other standalone tests:')
+    xpass = glob(join(TEST_DIR, '**/*.wasm'), recursive=True)
+    xpass_result = _run_wast_tests(engine, xpass, False)
+
+    tests_total = len(xpass)
+    fail_total = xpass_result
+    print('TOTAL: %d' % (tests_total))
+    print('%sPASS : %d%s' % (COLOR_GREEN, tests_total - fail_total, COLOR_RESET))
+    print('%sFAIL : %d%s' % (COLOR_RED, fail_total, COLOR_RESET))
+
+    if fail_total > 0:
+        raise Exception("other standalone tests failed")
+
+def run_from_other_file(engine, jit):
+    success, fail = [], []
+    for runner in RUNNERS:
+        print(COLOR_PURPLE + f'running test suite{ " with jit" if jit else ""}: ' + runner + COLOR_RESET)
+        try:
+            RUNNERS[runner](engine)
+            success += [runner]
+        except Exception as e:
+            print('\n'.join(COLOR_YELLOW + line + COLOR_RESET for line in traceback.format_exc().splitlines()))
+            fail += [runner]
+
+    if success:
+        print(COLOR_GREEN + sys.argv[0] + ': success: ' + ', '.join(success) + COLOR_RESET)
+    sys.exit(COLOR_RED + sys.argv[0] + ': fail: ' + ', '.join(fail) + COLOR_RESET if fail else None)
 
 def main():
     parser = ArgumentParser(description='Walrus Test Suite Runner')
