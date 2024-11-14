8271d4f8efd8d0ddb4879acabb0f39f63d396aa5
Ádám László Kulcsár <kuladam@inf.u-szeged.hu>
2024-11-14 13:49:53 +0100

    Delete proposal tests and merge run-tests.py and run-tests-other.py
    Signed-off-by: Ádám László Kulcsár <kuladam@inf.u-szeged.hu>

--- tools/run-tests.py
+++ tools/run-tests.py
@@ -31,8 +31,6 @@ from os.path import abspath, basename, dirname, join, relpath
 from shutil import copy
 from subprocess import PIPE, Popen
 
-other_tests = __import__('run-tests-other')
-
 PROJECT_SOURCE_DIR = dirname(dirname(abspath(__file__)))
 DEFAULT_WALRUS = join(PROJECT_SOURCE_DIR, 'walrus')
 
@@ -172,9 +170,86 @@ def run_extended_tests(engine):
     if fail_total > 0:
         raise Exception("wasm-test-extended failed")
 
-@runner('other', default=False)
-def run_other_tests(engine):
-    other_tests.run_from_other_file(engine, jit)
+@runner('regression', default=False)
+def run_extended_tests(engine):
+    TEST_DIR = join(PROJECT_SOURCE_DIR, 'test', 'other')
+
+
+    other_test_run_config = json.load(open(join(TEST_DIR, 'regression/running_config.json')))
+
+    test_list_pass = []
+    test_list_fail = []
+
+    for test_case in other_test_run_config['test cases']:
+        if test_case['deprecated']:
+            continue
+
+        test_paths = sum(
+            [glob(join(TEST_DIR, 'regression', f'issue-{id}', '*.wasm'), recursive=False) for id in test_case['ids']],
+            [])
+
+        if not test_paths:
+            test_paths = sum(
+                [glob(join(TEST_DIR, 'regression', f'issue-{id}', f"{test_case['file']}"), recursive=False) for id in
+                 test_case['ids']],
+                [])
+
+        if ('expected return' not in test_case and test_case['compile_options']['expected return']['ret code'] == 0) \
+                or test_case['expected return']['ret code'] == 0:
+            test_list_pass.extend(test_paths)
+        else:
+            test_list_fail.extend(test_paths)
+
+    print('Running other regression tests:')
+    result = _run_wast_tests(engine, test_list_pass, False)
+    result += _run_wast_tests(engine, test_list_fail, True)
+
+    tests_total = len(test_list_pass) + len(test_list_fail)
+    fail_total = result
+    print('TOTAL: %d' % (tests_total))
+    print('%sPASS : %d%s' % (COLOR_GREEN, tests_total - fail_total, COLOR_RESET))
+    print('%sFAIL : %d%s' % (COLOR_RED, fail_total, COLOR_RESET))
+
+    if fail_total > 0:
+        raise Exception("other regression failed")
+
+@runner('unit', default=True)
+def run_other_unit_tests(engine):
+    TEST_DIR = join(PROJECT_SOURCE_DIR, 'test', 'other', 'unit')
+
+    print('Running other-unit tests:')
+    xfail = [filename for filename in glob(join(TEST_DIR, 'linear-memory-wasm/*.wast'), recursive=False) if
+             ";; Should report an error." in open(filename).read()]
+    xfail.extend([filename for filename in glob(join(TEST_DIR, 'memory64/*exceed*.wat'), recursive=False)])
+    xpass = [filename for filename in glob(join(TEST_DIR, '**/*.wa*t'), recursive=True) if filename not in xfail]
+    xpass_result = _run_wast_tests(engine, xpass, False)
+    xpass_result += _run_wast_tests(engine, xfail, True)
+
+    tests_total = len(xpass) + len(xfail)
+    fail_total = xpass_result
+    print('TOTAL: %d' % (tests_total))
+    print('%sPASS : %d%s' % (COLOR_GREEN, tests_total - fail_total, COLOR_RESET))
+    print('%sFAIL : %d%s' % (COLOR_RED, fail_total, COLOR_RESET))
+
+    if fail_total > 0:
+        raise Exception("other unit failed")
+
+@runner('standalone', default=True)
+def run_extended_tests(engine):
+    TEST_DIR = join(PROJECT_SOURCE_DIR, 'test', 'other', 'standalone')
+
+    print('Running other standalone tests:')
+    xpass = glob(join(TEST_DIR, '**/*.wasm'), recursive=True)
+    xpass_result = _run_wast_tests(engine, xpass, False)
+
+    tests_total = len(xpass)
+    fail_total = xpass_result
+    print('TOTAL: %d' % (tests_total))
+    print('%sPASS : %d%s' % (COLOR_GREEN, tests_total - fail_total, COLOR_RESET))
+    print('%sFAIL : %d%s' % (COLOR_RED, fail_total, COLOR_RESET))
+
+    if fail_total > 0:
+        raise Exception("other standalone tests failed")
 
 def main():
     parser = ArgumentParser(description='Walrus Test Suite Runner')
