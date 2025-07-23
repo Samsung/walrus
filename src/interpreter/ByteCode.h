c29bb1fcdf2316ca0fb1c6f80a5a1459582ba54d
Ádám Kulcsár <kuladam@inf.u-szeged.hu>
2025-07-23 15:48:12 +0200

    Patch bugs on x64 and x86
    Signed-off-by: Ádám Kulcsár <kuladam@inf.u-szeged.hu>

--- src/interpreter/ByteCode.h
+++ src/interpreter/ByteCode.h
@@ -1763,6 +1763,11 @@ public:
     {
     }
 
+    void setDstOffset(Walrus::ByteCodeStackOffset o)
+    {
+        m_dstOffset = o;
+    }
+
     ByteCodeStackOffset dstOffset() const { return m_dstOffset; }
     uint16_t memIndex() const { return m_memIndex; }
 
@@ -1992,9 +1997,13 @@ public:
 
     uint32_t offset() const { return m_offset; }
     ByteCodeStackOffset index() const { return m_index; }
+    void setIndex(ByteCodeStackOffset o) { m_index = o; }
     ByteCodeStackOffset src0Offset() const { return m_src0Offset; }
+    void setSrc0Offset(ByteCodeStackOffset o) { m_src0Offset = o; }
     ByteCodeStackOffset src1Offset() const { return m_src1Offset; }
+    void setSrc1Offset(ByteCodeStackOffset o) { m_src1Offset = o; }
     ByteCodeStackOffset dstOffset() const { return m_dstOffset; }
+    void setDstOffset(ByteCodeStackOffset o) { m_dstOffset = o; }
 
 #if !defined(NDEBUG)
     void dump(size_t pos)
@@ -2026,9 +2035,13 @@ public:
 
     uint32_t offset() const { return m_offset; }
     ByteCodeStackOffset index() const { return m_index; }
+    void setIndex(ByteCodeStackOffset o) { m_index = o; }
     ByteCodeStackOffset src0Offset() const { return m_src0Offset; }
+    void setSrc0Offset(ByteCodeStackOffset o) { m_src0Offset = o; }
     ByteCodeStackOffset src1Offset() const { return m_src1Offset; }
+    void setSrc1Offset(ByteCodeStackOffset o) { m_src1Offset = o; }
     ByteCodeStackOffset dstOffset() const { return m_dstOffset; }
+    void setDstOffset(ByteCodeStackOffset o) { m_dstOffset = o; }
     uint16_t memIndex() const { return m_memIndex; }
     uint16_t alignment() const { return m_alignment; }
 
@@ -2181,8 +2194,11 @@ public:
 
     uint32_t offset() const { return m_offset; }
     ByteCodeStackOffset index() const { return m_index; }
+    void setIndex(Walrus::ByteCodeStackOffset o) { m_index = o; }
     ByteCodeStackOffset src0Offset() const { return m_src0Offset; }
+    void setSrc0Offset(ByteCodeStackOffset o) { m_src0Offset = o; }
     ByteCodeStackOffset src1Offset() const { return m_src1Offset; }
+    void setSrc1Offset(ByteCodeStackOffset o) { m_src1Offset = o; }
 
 #if !defined(NDEBUG)
     void dump(size_t pos)
@@ -2212,8 +2228,11 @@ public:
 
     uint32_t offset() const { return m_offset; }
     ByteCodeStackOffset index() const { return m_index; }
+    void setIndex(Walrus::ByteCodeStackOffset o) { m_index = o; }
     ByteCodeStackOffset src0Offset() const { return m_src0Offset; }
+    void setSrc0Offset(Walrus::ByteCodeStackOffset o) { m_src0Offset = o; }
     ByteCodeStackOffset src1Offset() const { return m_src1Offset; }
+    void setSrc1Offset(Walrus::ByteCodeStackOffset o) { m_src1Offset = o; }
     uint16_t memIndex() const { return m_memIndex; }
     uint16_t alignment() const { return m_alignment; }
 
@@ -2243,8 +2262,11 @@ public:
     }
 
     ByteCodeStackOffset index() const { return m_index; }
+    void setIndex(ByteCodeStackOffset o) { m_index = o; }
     ByteCodeStackOffset srcOffset() const { return m_srcOffset; }
+    void setSrcOffset(ByteCodeStackOffset o) { m_srcOffset = o; }
     ByteCodeStackOffset dstOffset() const { return m_dstOffset; }
+    void setDstOffset(ByteCodeStackOffset o) { m_dstOffset = o; }
 
 #if !defined(NDEBUG)
     void dump(size_t pos)
@@ -2269,8 +2291,12 @@ public:
     }
 
     uint32_t index() const { return m_index; }
+    void setIndex(ByteCodeStackOffset o) { m_index = o; }
     const ByteCodeStackOffset* srcOffsets() const { return m_srcOffsets; }
+    void setSrc0Offset(ByteCodeStackOffset o) { m_srcOffsets[0] = o; }
+    void setSrc1Offset(ByteCodeStackOffset o) { m_srcOffsets[1] = o; }
     ByteCodeStackOffset dstOffset() const { return m_dstOffset; }
+    void setDstOffset(ByteCodeStackOffset o) { m_dstOffset = o; }
 
 #if !defined(NDEBUG)
     void dump(size_t pos)
--- src/runtime/Value.h
+++ src/runtime/Value.h
@@ -21,6 +21,8 @@
 #include "util/BitOperation.h"
 #include "runtime/ExecutionState.h"
 #include "runtime/Exception.h"
+#include <cmath>
+#include <limits>
 
 namespace Walrus {
 
@@ -161,7 +163,7 @@ public:
     }
 
     Value(Type type)
-        : m_i64(0)
+        : m_v128()
         , m_type(type)
     {
     }
@@ -351,19 +353,24 @@ public:
     {
         switch (m_type) {
         case I32:
-            return m_f32 == 0;
-        case F32:
             return m_i32 == 0;
-        case F64:
-            return m_f64 == 0;
+        case F32:
+            return m_f32 == 0.0f && !std::signbit(m_f32);
         case I64:
             return m_i64 == 0;
+        case F64:
+            return m_f64 == +0.0 && !std::signbit(m_f64);
         case FuncRef:
         case ExternRef:
             return m_ref == nullptr;
-        case V128:
-            uint8_t empty[16];
-            return m_v128.m_data == empty;
+        case V128: {
+            for (uint8_t i = 0; i < 16; i++) {
+                if (m_v128.m_data[i] != 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
         default:
             ASSERT_NOT_REACHED();
             break;
