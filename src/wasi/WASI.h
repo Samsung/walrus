d2e3c3f6e4fe7cfbda4ca437ba5e15d50d8c64a1
Luca388 <dkdls388@gmail.com>
2026-07-10 20:18:04 +0900

    [WASI] Implement fd_allocate

--- src/wasi/WASI.h
+++ src/wasi/WASI.h
@@ -44,6 +44,7 @@ public:
     F(clock_time_get, I32I64I32_RI32)                      \
     F(random_get, I32I32_RI32)                             \
     F(fd_pwrite, I32I32I32I64I32_RI32)                     \
+    F(fd_allocate, I32I64I64_RI32)                         \
     F(fd_write, I32I32I32I32_RI32)                         \
     F(fd_tell, I32I32_RI32)                                \
     F(fd_read, I32I32I32I32_RI32)                          \
