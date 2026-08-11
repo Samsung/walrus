517245d2bcb20ad4fc16e60763fa36fc4a9b7d9e
M-SE0K <seg082911@gmail.com>
2026-08-11 12:29:30 +0900

    refactor: Simplify duplicate opcode address detection
    Signed-off-by: M-SE0K <seg082911@gmail.com>

--- src/interpreter/Interpreter.cpp
+++ src/interpreter/Interpreter.cpp
@@ -474,16 +474,11 @@ inline static void simdSplatOperation(ExecutionState& state, UnaryOperation* cod
 #if defined(WALRUS_ENABLE_COMPUTED_GOTO)
 static void initAddressToOpcodeTable()
 {
-#define REGISTER_TABLE(name, ...)                                                                     \
-    {                                                                                                  \
-        void* addr = g_byteCodeTable.m_addressTable[ByteCode::name##Opcode];                          \
-        auto it = g_byteCodeTable.m_addressToOpcodeTable.find(addr);                                   \
-        RELEASE_ASSERT(it == g_byteCodeTable.m_addressToOpcodeTable.end()                              \
-                       || it->second == ByteCode::name##Opcode);                                       \
-        g_byteCodeTable.m_addressToOpcodeTable[addr] = ByteCode::name##Opcode;                         \
-    }
+#define REGISTER_TABLE(name, ...) \
+    g_byteCodeTable.m_addressToOpcodeTable[g_byteCodeTable.m_addressTable[ByteCode::name##Opcode]] = ByteCode::name##Opcode;
     FOR_EACH_BYTECODE(REGISTER_TABLE)
 #undef REGISTER_TABLE
+    RELEASE_ASSERT(g_byteCodeTable.m_addressToOpcodeTable.size() == ByteCode::OpcodeKindEnd);
 }
 #endif
 
